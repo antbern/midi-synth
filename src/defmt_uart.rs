40adb143851afa3bf9abcc6cc2fabffc9d95b8fc
antbern <40672068+antbern@users.noreply.github.com>
2022-03-26 19:44:01 +0100

    Change `defmt` to transport via UART0 instead of RTT and add vscode task to start `defmt-print`

---
+++ src/defmt_uart.rs
@@ -0,0 +1,77 @@
+/// Module contains an implementation of `defmt::Logger` that prints to the UART.
+/// Based heavily on the `defmt-rtt` implementation for various versions, including
+/// [0.2.0](https://docs.rs/defmt-rtt/0.2.0/src/defmt_rtt/lib.rs.html#25-64) and
+/// [0.3.2](https://docs.rs/defmt-rtt/0.3.2/src/defmt_rtt/lib.rs.html#35-83)
+///
+use core::sync::atomic::{AtomicBool, Ordering};
+
+use cortex_m::{
+    interrupt::{self, free},
+    register,
+};
+
+use crate::GLOBAL_UART0;
+
+#[defmt::global_logger]
+struct UartLogger;
+
+/// Global logger lock.
+static TAKEN: AtomicBool = AtomicBool::new(false);
+static INTERRUPTS_ACTIVE: AtomicBool = AtomicBool::new(false);
+static mut ENCODER: defmt::Encoder = defmt::Encoder::new();
+
+unsafe impl defmt::Logger for UartLogger {
+    fn acquire() {
+        // get interrupt active status before disabling interrupts
+        let primask = register::primask::read();
+        // SAFETY: must be paired with a call to enable() (if applicable)
+        interrupt::disable();
+
+        if TAKEN.load(Ordering::Relaxed) {
+            panic!("defmt logger taken reentrantly")
+        }
+
+        // no need for CAS because interrupts are disabled
+        TAKEN.store(true, Ordering::Relaxed);
+
+        INTERRUPTS_ACTIVE.store(primask.is_active(), Ordering::Relaxed);
+
+        // safety: accessing the `static mut` is OK because we have disabled interrupts.
+        unsafe { ENCODER.start_frame(do_write) }
+    }
+
+    unsafe fn flush() {
+        // SAFETY: if we get here, the global logger mutex is currently acquired
+
+        // no flush needed (only blocking IO for now)
+    }
+
+    unsafe fn release() {
+        // safety: accessing the `static mut` is OK because we have disabled interrupts.
+        ENCODER.end_frame(do_write);
+
+        TAKEN.store(false, Ordering::Relaxed);
+
+        // safety: Must be paired with corresponding call to disable(), see above
+        if INTERRUPTS_ACTIVE.load(Ordering::Relaxed) {
+            interrupt::enable();
+        }
+    }
+
+    unsafe fn write(bytes: &[u8]) {
+        // safety: accessing the `static mut` is OK because we have disabled interrupts.
+        ENCODER.write(bytes, do_write);
+    }
+}
+
+fn do_write(bytes: &[u8]) {
+    // write to the UART here
+    free(|cs| {
+        GLOBAL_UART0
+            .borrow(cs)
+            .borrow()
+            .as_ref()
+            .unwrap()
+            .write_full_blocking(bytes);
+    })
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 #![no_std]
 #![no_main]
 
+mod defmt_uart;
+
+use core::cell::RefCell;
+
+use cortex_m::interrupt::{self, Mutex};
 use cortex_m_rt::entry;
 use defmt::*;
-use defmt_rtt as _;
 use embedded_hal::digital::v2::OutputPin;
 use embedded_time::fixed_point::FixedPoint;
 use panic_probe as _;
@@ -11,14 +15,32 @@ use panic_probe as _;
 use rp_pico as bsp;
 
 use bsp::hal::{
+    self,
     clocks::{init_clocks_and_plls, Clock},
+    gpio::{
+        bank0::{Gpio0, Gpio1},
+        FunctionUart,
+    },
     pac,
     sio::Sio,
+    uart::{self, UartPeripheral},
     watchdog::Watchdog,
 };
+
+/// Alias the type for our UART pins to make things clearer.
+type UartPins = (
+    hal::gpio::Pin<Gpio0, hal::gpio::Function<hal::gpio::Uart>>,
+    hal::gpio::Pin<Gpio1, hal::gpio::Function<hal::gpio::Uart>>,
+);
+
+/// Alias the type for our UART to make things clearer.
+type Uart = hal::uart::UartPeripheral<hal::uart::Enabled, pac::UART0, UartPins>;
+
+/// Shared UART object so that it can be accessed from defmt
+static GLOBAL_UART0: Mutex<RefCell<Option<Uart>>> = Mutex::new(RefCell::new(None));
+
 #[entry]
 fn main() -> ! {
-    info!("Program start");
     let mut pac = pac::Peripherals::take().unwrap();
     let core = pac::CorePeripherals::take().unwrap();
     let mut watchdog = Watchdog::new(pac.WATCHDOG);
@@ -48,6 +70,28 @@ fn main() -> ! {
     );
 
     let mut led_pin = pins.led.into_push_pull_output();
+
+    let uart_pins = (
+        pins.gpio0.into_mode::<FunctionUart>(),
+        pins.gpio1.into_mode::<FunctionUart>(),
+    );
+
+    let uart = UartPeripheral::new(pac.UART0, uart_pins, &mut pac.RESETS)
+        .enable(
+            uart::common_configs::_115200_8_N_1,
+            clocks.peripheral_clock.into(),
+        )
+        .unwrap();
+
+    uart.write_full_blocking(b"Hello World!=n");
+
+    // put the UART into the shared global
+    interrupt::free(|cs| {
+        GLOBAL_UART0.borrow(cs).replace(Some(uart));
+    });
+
+    // UART is now initialized, and we can start using defmt macros!
+    info!("Program start");
 
     loop {
         info!("on!");
