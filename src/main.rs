02cef9d76934c76c9f6f9d59994a80f70054f9b7
antbern <40672068+antbern@users.noreply.github.com>
2022-05-07 17:28:04 +0200

    Initial test of I2S audio output to the MAX98357A amplifier
    Builds on the `audio_i2s` example in `pico-extras`, which is where
    the PIO code is taken from.
    
    https://github.com/raspberrypi/pico-extras/blob/master/src/rp2_common/pico_audio_i2s/audio_i2s.c

--- src/main.rs
+++ src/main.rs
@@ -19,9 +19,10 @@ use bsp::hal::{
     clocks::{init_clocks_and_plls, Clock},
     gpio::{
         bank0::{Gpio0, Gpio1},
-        FunctionUart,
+        FunctionPio0, FunctionUart, Pin,
     },
     pac,
+    pio::{Buffers, PIOExt},
     sio::Sio,
     uart::{self, UartPeripheral},
     watchdog::Watchdog,
@@ -69,8 +70,6 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
-    let mut led_pin = pins.led.into_push_pull_output();
-
     let uart_pins = (
         pins.gpio0.into_mode::<FunctionUart>(),
         pins.gpio1.into_mode::<FunctionUart>(),
@@ -93,12 +92,111 @@ fn main() -> ! {
     // UART is now initialized, and we can start using defmt macros!
     info!("Program start");
 
+    // DATA: Pin GPIO2
+    // BCLK: Pin GPIO3
+    // LRCLCK: Pin GPIO4
+
+    let data_pin = 2;
+    let bclk_pin = 3;
+    let lrclk_pin = 4;
+
+    // configure pins for Pio0
+    let _data: Pin<_, FunctionPio0> = pins.gpio2.into_mode();
+    let _bclk: Pin<_, FunctionPio0> = pins.gpio3.into_mode();
+    let _lrclk: Pin<_, FunctionPio0> = pins.gpio4.into_mode();
+
+    // Create and start the I2S pio program
+    let program = pio_proc::pio_file!("audio_i2s.pio", options());
+    let _s = program.public_defines.entry_point;
+
+    // Initialize and start PIO
+    let (mut pio, sm0, _, _, _) = pac.PIO0.split(&mut pac.RESETS);
+    let installed = pio.install(&program.program).unwrap();
+
+    let _d = installed.wrap_target();
+
+    debug!("sysclk = {:?}", clocks.system_clock.freq().0);
+
+    let sample_freq: u32 = 16_000; //32_000; // 16khz
+
+    let system_clock_frequency = clocks.system_clock.freq().0;
+
+    core::assert!(system_clock_frequency < 0x40000000);
+    let divider = system_clock_frequency * 4 / sample_freq; // avoid arithmetic overflow
+    core::assert!(divider < 0x1000000);
+
+    debug!("divider >> 8 = {:?}", divider >> 8);
+    debug!("divider & 0xff = {:?}", divider & 0xff);
+
+    // construct the input the clock_divisor() call expects...
+    let divisor = (divider >> 8) as f32 + (divider & 0xff) as f32 / 256.0;
+
+    // just double check with the logic from clock_divisor()
+    let int = divisor as u16;
+    let frac = ((divisor - int as f32) * 256.0) as u8;
+
+    debug!("int = {:?}", int);
+    debug!("frac = {:?}", frac);
+
+    let (mut sm, _rx, mut tx) = hal::pio::PIOBuilder::from_program(installed)
+        .side_set_pin_base(bclk_pin)
+        .out_pins(data_pin, 1)
+        .out_shift_direction(hal::pio::ShiftDirection::Left)
+        .autopull(true)
+        .pull_threshold(32)
+        .clock_divisor(divisor)
+        .buffers(Buffers::OnlyTx)
+        .build(sm0);
+
+    // The GPIO pins need to be configured as an output.
+    sm.set_pindirs([
+        (data_pin, hal::pio::PinDir::Output),
+        (bclk_pin, hal::pio::PinDir::Output),
+        (lrclk_pin, hal::pio::PinDir::Output),
+    ]);
+
+    sm.exec_instruction(
+        pio::InstructionOperands::JMP {
+            condition: pio::JmpCondition::Always,
+            address: 0x18 + 0x7 as u8,
+        }
+        .encode(),
+    );
+
+    sm.start();
+
+    let mut led_pin = pins.led.into_push_pull_output();
+
+    //generate a square wave output
+    let freq = 440 * 2;
+
+    let sample_counter_wrap = sample_freq / freq;
+    let mut sample_counter = 0u32;
     loop {
-        info!("on!");
-        led_pin.set_high().unwrap();
-        delay.delay_ms(500);
-        info!("off!");
-        led_pin.set_low().unwrap();
-        delay.delay_ms(500);
+        let sample = if sample_counter < sample_counter_wrap / 2 {
+            i16::MAX
+        } else {
+            i16::MIN
+        };
+
+        // let sample = 0u32;
+        //let lr_sample: u32 = ((sample as u16 as u32) << 16) | (sample as u16) as u32;
+        // debug!("{:x}", sample);
+
+        // push samples but do not advance if buffer is full
+        if tx.write(sample >> 8) {
+            sample_counter = (sample_counter + 1) % sample_counter_wrap;
+            led_pin.set_high().unwrap();
+        } else {
+            led_pin.set_low().unwrap();
+        }
+
+        // tx.write(0u32);
+        // info!("on!");
+
+        // delay.delay_ms(500);
+        // info!("off!");
+
+        // delay.delay_ms(500);
     }
 }
